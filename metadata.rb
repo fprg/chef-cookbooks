cdc8b0b216ae271eab857ad8598252777232cabb
furuwata <furuwata@cloudpack.jp>
2015-12-17 18:38:57 +0900

    metadataの追加

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'fprg-chef-cookbooks'
+maintainer       'f_prg'
+maintainer_email 'f_prg'
+license          'All rights reserved'
+description      'chef repo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
